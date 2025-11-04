6e6b0238940e8017bdc89e4a161da643a86df09d
Patel Shubham <87634631+N3xus97@users.noreply.github.com>
2025-11-04 11:13:12 +0530

    added user aproval api

---
+++ src/controller/admin/adminContoller.js
@@ -0,0 +1,29 @@
+const { error } = require('console')
+const { query } = require('../../../config/dbConfig')
+const AppError = require('../../../utils/appError');
+const logger = require('../../../utils/logger');
+
+
+const approveProfile = async (req, res, next) => {
+    try {
+        const { userEmail } = req.body;
+        const adminUser = decodedToken(req.cookies?.AccessToken);
+        await query(
+            `UPDATE users
+   SET approval_status = 'approved',
+       approved_at = $1
+   WHERE user_email = $2`,
+            [new Date(), userEmail]
+        );
+        logger.info(`user ${userEmail} approved by admin ${adminUser.name}`)
+
+        return res.status(200).json({
+            status: 'success',
+            message: ' user approval successful'
+        });
+
+    } catch (error) {
+        logger.error(error)
+        return next(new AppError(`failed to update user approval`, 500))
+    }
+}
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser,otpSendApi,verifyOtpAndProcess } = require('../controller');
+const { loginUser, otpSendApi, verifyOtpAndProcess } = require('../controller');
 const { authenticateUser, refreshAccessToken, logout } = require('../middleware/authMiddleware');
 const { setTokenCookies } = require('../middleware/tokenCookieMiddleware');
-// const { isFirstTime } = require('../controller/auth/isFirstTime/isFirstTimeController')
 const { logger } = require('../../utils/logger');
 const upload = require('../../config/multer');
 
 router.post("/send-otp", otpSendApi);
-router.post("/verify-otp",upload.single('file'), verifyOtpAndProcess);
+router.post("/verify-otp", upload.single('file'), verifyOtpAndProcess);
 router.post("/login", loginUser, setTokenCookies, (req, res) => {
 
   res.status(200).json({
     message: "Login successful ",
-    // tokensCookieSet: true,
+    tokensCookieSet: true,
   });
 
 });
@@ -22,29 +21,7 @@ router.post('/refresh', refreshAccessToken, (req, res) => {
   res.status(200).json({ message: 'Access token refreshed' });
 });
 
-// Profile Chage Password 
-// router.post("/changePassword", authenticateUser, changePasswordController);
-// router.get("/delete-user-request", authenticateUser, deleteUserRequestController);
-// router.post("/delete-user", authenticateUser, deleteUserExecutionController);
-// router.get('/delete-user-list', authenticateUser, getDeleteUserListController);
-
-
 router.post('/logout', authenticateUser, logout);
-// router.post('/register', registerUser)
-
-// router.post('/block-user', authenticateUser, blockUserController);
-// router.post('/unblock-user', authenticateUser, unblockUserController);
-
-// router.get('/isFirstTime', authenticateUser, isFirstTime);
-
-// router.post('/google-signin', googleLoginUser, setTokenCookies, (req, res) => {
-//   res.status(200).json({
-//     message: "Google login successful",
-//     tokensCookieSet: true,
-//     accessToken: res.locals.accessToken,
-//     refreshAccessToken: res.locals.refreshToken
-//   });
-// });
 
 
 module.exports = router;
