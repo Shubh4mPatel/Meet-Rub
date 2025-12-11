90b99e7250b8be449eb5efb61fb04abf74f1bac8
Patel Shubham <87634631+N3xus97@users.noreply.github.com>
2025-12-11 15:17:36 +0530

    Refactor Redis client configuration, add Docker setup for chat server and Redis service, and create .dockerignore file

--- chat-server/config/reddis.js
+++ chat-server/config/reddis.js
@@ -3,13 +3,19 @@ const redis = require('redis');
 
 
 const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: 6379,
-    password: process.env.REDIS_PASSWORD 
+    socket: {
+        host: process.env.REDIS_HOST || '127.0.0.1',
+        port: parseInt(process.env.REDIS_PORT) || 6379
+    },
+    password: process.env.REDIS_PASSWORD
 });
+
 redisClient.on("error", (err) => console.error("Redis error:", err));
 
 redisClient.connect().then(() => {
   console.log("✅ Connected to Redis");
+}).catch((err) => {
+  console.error("❌ Failed to connect to Redis:", err);
 });
+
 module.exports = redisClient;
