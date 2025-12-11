f181d3acdf60e8faa82d21e2dca0fab20712ad48
Patel Shubham <87634631+N3xus97@users.noreply.github.com>
2025-12-11 16:22:13 +0530

    `Added payment and wallet routes to index.js`

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,8 @@ const userProfileRoutes = require('./userProfileRoute');
 const adminRoutes = require('./adminRoutes')
 const freelancerRoutes = require('./freelancerRoutes')
 const creatorRoutes = require('./creatorRoutes')
+const paymentRoutes = require('./paymentRoutes');
+const walletRoutes = require('./walletRoutes');
 const { authenticateUser, requireRole } = require('../middleware/authMiddleware');
 
 router.use('/auth', authroutes);
@@ -12,5 +14,8 @@ router.use('/user-profile', authenticateUser, userProfileRoutes);
 router.use('/admin', authenticateUser, requireRole(['admin']), adminRoutes)
 router.use('/freelancer', authenticateUser, requireRole(['freelancer']), freelancerRoutes)
 router.use('/creator', authenticateUser, requireRole(['creator']), creatorRoutes)
+router.use('/payments',authenticateUser, paymentRoutes);
+router.use('/wallet', authenticateUser, walletRoutes);
+
 
 module.exports = router;
