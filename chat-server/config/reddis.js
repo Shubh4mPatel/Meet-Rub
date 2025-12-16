f524e0ecd55b336ce87d424ddc768b345b584eed
Shubham <shubh4m@dev@gmail.com>
2025-12-17 00:47:53 +0530

    fix: Change Redis port from 6379 to 6378 in configuration and Docker setup

--- chat-server/config/reddis.js
+++ chat-server/config/reddis.js
@@ -5,7 +5,7 @@ const redis = require('redis');
 const redisClient = redis.createClient({
     socket: {
         host: process.env.REDIS_HOST || '127.0.0.1',
-        port: parseInt(process.env.REDIS_PORT) || 6379
+        port: parseInt(process.env.REDIS_PORT) || 6378
     },
     password: process.env.REDIS_PASSWORD
 });
