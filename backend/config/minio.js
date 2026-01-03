3735a96bd4bddda520609c5bc5bc02e52af0fc99
Shubham <shubh4m@dev@gmail.com>
2026-01-03 18:29:25 +0530

    chgaing docker

--- backend/config/minio.js
+++ backend/config/minio.js
@@ -12,13 +12,13 @@ const minioClient = new Client({
   secretKey: process.env.MINIO_SECRET_KEY
 });
 
-const minioPublicClient = new Client({
-  endPoint: process.env.MINIO_PUBLIC_ENDPOINT , // Public IP or domain
-  port: parseInt(process.env.MINIO_PUBLIC_PORT) || 9000,
-  useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: process.env.MINIO_SECRET_KEY
-});
+// const minioPublicClient = new Client({
+//   endPoint: process.env.MINIO_PUBLIC_ENDPOINT , // Public IP or domain
+//   port: parseInt(process.env.MINIO_PUBLIC_PORT) || 9000,
+//   useSSL: false,
+//   accessKey: process.env.MINIO_ACCESS_KEY,
+//   secretKey: process.env.MINIO_SECRET_KEY
+// });
 // const BUCKET_NAME = process.env.MINIO_BUCKET_NAME || 'media-uploads';
 
 // Ensure bucket exists
