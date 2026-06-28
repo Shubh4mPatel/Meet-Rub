ec561388a199024433c41439a8f8fbdcda8942fa
Shubh4m <shubh4m.dev@gmail.com>
2026-06-28 16:52:07 +0530

    ..

--- migrations/add_gov_id_back_url.sql
+++
@@ -1,6 +0,0 @@
--- Adds a column to store the Aadhar (govt ID) BACK image path.
--- The front image continues to use freelancer.gov_id_url.
--- Run once against the database.
-
-ALTER TABLE public.freelancer
-  ADD COLUMN IF NOT EXISTS gov_id_back_url text;
